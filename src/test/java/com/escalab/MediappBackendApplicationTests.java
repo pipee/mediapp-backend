3bb698649f878db4a58127ee1b5fb6ccc264ffa3
Luis Felipe Armijo <luis.armijo@axity.com>
2020-11-05 21:04:36 -0300

    test unitario

--- src/test/java/com/escalab/MediappBackendApplicationTests.java
+++ src/test/java/com/escalab/MediappBackendApplicationTests.java
@@ -1,13 +1,36 @@
 package com.escalab;
 
+import com.escalab.model.Usuario;
+import com.escalab.repo.IUsuarioRepo;
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class MediappBackendApplicationTests {
+public class MediappBackendApplicationTests {
+
+	@Autowired
+	private BCryptPasswordEncoder bcrypt;
+
+	@Autowired
+	private IUsuarioRepo repo;
 
 	@Test
-	void contextLoads() {
+	public void crearUsuario() {
+		Usuario us = new Usuario();
+		us.setIdUsuario(0);
+		us.setUsername("ingfelipe");
+		us.setPassword(bcrypt.encode("123"));
+		us.setEnabled(true);
+
+		Usuario retorno = repo.save(us);
+
+		Assert.assertTrue(retorno.getPassword().equalsIgnoreCase(us.getPassword()));
 	}
 
 }
